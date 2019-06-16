7e2c413aac97756fbdc189b59021501fc428259a
Pramodini <pramodinip677@gmail.com>
2019-06-16 11:02:46 -0700

    book table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Container} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
 import BookList from './Components/bookList'
 
 function App() {
   return (
-    <div className="App">
-      WELCOME
+    <Container>
+      <h1 className="my-2 text-primary">GOQUO</h1>
+      <h6 className="mb-5 text-info font-italic">Get the best Hotel at best Price </h6>
       <BookList/> 
-    </div>
+    </Container>
   );
 }
 
--- src/Components/bookList.js
+++ src/Components/bookList.js
@@ -1,17 +1,23 @@
-
-/*eslint no-use-before-define: ["error", { "variables": false }]*/
 import React, { Component } from 'react'
 import bookDataList from '../mock/getHotelBookData';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import './bookList.css';
+
 
 export default class bookList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       bookDataList : bookDataList,
-      groupBookDataList: bookDataList
+      groupBookDataList: {}
     };
+    this.onClickAction = this.onClickAction.bind(this);
   }
 
+  onClickAction(room, bed, desc, price) {
+   alert(room + ', '  + bed + ', ' + desc + ', ' + price);
+  }
 
   componentDidMount() {
     let {bookDataList} = this.state;
@@ -29,31 +35,54 @@ export default class bookList extends Component {
       acc[item.roomTypeLabel].push(item);
       return acc;
     },{})
-    //const groupArray = Object.entries(group).map((e) => ( { [e[0]]: e[1] } ));
-   // console.log("list" + abc);
-    this.setState({groupBookDataList: group})
+   this.setState({groupBookDataList: group})
   }
 
   render() {
     const {groupBookDataList} = this.state;
-
     return (
-        <div>
-            {
-              Object.keys(groupBookDataList).map(name => {
-                return (
-                  <div>
-                    <div>{name}</div>
-                    {
-                      groupBookDataList[name].map(data => {
-                        return <div>{data.totalPrice}</div> 
-                      })
-                    }
-                  </div>
-                )
+       <Container>
+          <Row className="block-example border bg-info text-center font-weight-bold pb-1 text-white text-capitalize">
+               <Col xs={12} sm={4} className="pt-1 pb-1">
+                  Room Type
+                </Col>
+                <Col xs={12} sm={4} className=" pt-1 pb-1">
+                  Bed Type
+                </Col>
+                <Col xs={12} sm={4} className="pt-1 pb-1">
+                  Price
+                </Col>
+          </Row>
+          {  
+            Object.keys(groupBookDataList).map(name => {
+              return  Object.keys(groupBookDataList[name]).map((iter)=>{
+                  return (
+                    <Row className="block-example mb-3 border border-info text-center text-capitalize">
+                       <Col xs={12} sm={4} className="border-right pt-3 pb- border-info">
+                           {name}
+                       </Col>
+                       <Col xs={12} sm={4}  className="border-right pt-3 pb-1 border-info">
+                          <ul className="list-column">
+                            <li>
+                              {groupBookDataList[name][iter].bedTypeLabel}
+                            </li>
+                            <li>
+                            {groupBookDataList[name][iter].boardCodeDescription}
+                            </li>
+                          </ul>
+                       </Col>
+                       <Col xs={12} sm={4} className="pt-3 pb-1">
+                           <span className="mr-3">{groupBookDataList[name][iter].totalPrice}</span>
+                           <Button variant="info" size="sm" onClick={(e) =>this.onClickAction(name, groupBookDataList[name][iter].bedTypeLabel , groupBookDataList[name][iter].boardCodeDescription , groupBookDataList[name][iter].totalPrice)}>
+                             Action
+                            </Button>
+                       </Col>
+                    </Row>
+                  )
               })
+            })
             }
-      </div>
+       </Container>
     )
   }
 }
